a239128b63a4f462f8416db6205867d8bdc666a7
Prince Verma <16836599+vprince001@users.noreply.github.com>
2019-04-02 15:27:28 +0530

    Updates

---
+++ precache-manifest.d2981dc1ed7e530352047a3fa410f703.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "823ceae628c968c44b0a",
+    "url": "/calculator-react/static/js/runtime~main.b9732b27.js"
+  },
+  {
+    "revision": "965000d08dd0fbd42297",
+    "url": "/calculator-react/static/js/main.70a8873b.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/calculator-react/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "965000d08dd0fbd42297",
+    "url": "/calculator-react/static/css/main.2739f9f1.chunk.css"
+  },
+  {
+    "revision": "129e52b1ea01f1672efa6058f7087a75",
+    "url": "/calculator-react/index.html"
+  }
+];
